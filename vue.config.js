6f00b1d87058d0cdd8ddfae9a4d48ced9cc92e6c
Tarae0419 <tarae0419@gmail.com>
2024-12-21 23:31:27 +0900

    수정

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: "/WSD2/",
-});
 module.exports = {
-  publicPath: "/WSD2/",
+
 };
