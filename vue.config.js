2a36ece5167f5f5af950c84fb01ea02d06d2a8aa
Tarae0419 <tarae0419@gmail.com>
2024-12-21 23:31:36 +0900

    Updates

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: "/WSD2/",
-});
-module.exports = {
-  publicPath: "/WSD2/",
-};
